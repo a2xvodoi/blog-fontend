e16a1d51774b45221a7b894ec94356ad205cb637
viet.nd <viet.nd@beetechsoft.vn>
2022-11-04 08:25:24 +0700

    add i18n

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ import store from "./store";
 // import css assets
 import "bootstrap/dist/css/bootstrap.css";
 
-const app = createApp(App);
+import i18n from './i18n'
+
+const app = createApp(App).use(i18n);
 app.use(router);
 app.use(store);
 app.mount("#app");
